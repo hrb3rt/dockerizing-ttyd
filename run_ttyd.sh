0ba7459327bd446a47f799f43607804cf573d8d0
Abhinand <abhinand4858@gmail.com>
2020-10-06 15:46:45 +0530

    Revert path

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "ttyd serving at port 80 with username:pass as 123:123"
-/usr/local/bin/ttyd_linux.x86_64 -p 80 -c 123:123 bash
+chmod +x /usr/local/bin/ttyd_linux
+/usr/local/bin/ttyd_linux -p 80 -c 123:123 bash
