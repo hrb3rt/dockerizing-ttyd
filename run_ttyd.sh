c2dc30b27b25c9f97c77fad4aefa4ee55001d584
hrb3rt <104071254+hrb3rt@users.noreply.github.com>
2022-04-21 20:57:10 +0200

    Update run_ttyd.sh

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "ttyd serving at port 80 with username:pass as 123:123"
+echo "ttyd serving at port $PORT with username:pass as 123:123"
 chmod +x /usr/local/bin/ttyd_linux
-/usr/local/bin/ttyd_linux -p 80 -c 123:123 bash
+/usr/local/bin/ttyd_linux -p $PORT -c 123:123 bash
