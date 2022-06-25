ae6ca1aecc5188f181e6da728967c07e5f2b4b15
hrb3rt <104071254+hrb3rt@users.noreply.github.com>
2022-06-26 01:39:40 +0200

    Update run_ttyd.sh

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -3,3 +3,4 @@
 echo "ttyd serving at port $PORT with username:pass as $USER:$PASSWORD"
 chmod +x /usr/local/bin/ttyd_linux
 /usr/local/bin/ttyd_linux -p $PORT -c $USER:$PASSWORD tmux
+echo "set -g mouse on" > ~/.tmux.conf
