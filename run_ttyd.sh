bba7e4419832e050c2599c0ec030aa4626d8304c
hrb3rt <104071254+hrb3rt@users.noreply.github.com>
2022-06-26 01:38:31 +0200

    Update run_ttyd.sh

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -2,4 +2,4 @@
 
 echo "ttyd serving at port $PORT with username:pass as $USER:$PASSWORD"
 chmod +x /usr/local/bin/ttyd_linux
-/usr/local/bin/ttyd_linux -p $PORT -c $USER:$PASSWORD bash
+/usr/local/bin/ttyd_linux -p $PORT -c $USER:$PASSWORD tmux
